0baa5ab51df6942ae357ca69d27742ea9dc42657
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-07 14:36:01 +0530

    JSX Restrictions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    // <div className="App">
-    //   <h1> Hi, I am a React App</h1>
-    // </div>
+    <div className="App">
+      <h1> Hi, I am a React App</h1>
+      <h1> You can add one more header here</h1>
+      <p>This is working great ! </p>
+    </div>
+    //<h1>Hi</h1> We can't do this here our JSX expression must have one root element
     //React.createElement('div', null, 'h1', 'Hi I am a React App')
-    React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Iam a React App')) //writing the code like this is cumbersome 
+    //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Iam a React App')) //writing the code like this is cumbersome 
     //Hence we choose above jsx code.
   );
 }
