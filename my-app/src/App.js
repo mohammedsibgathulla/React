c95fda8636a94665aca6796f2e4001bb8f941c47
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-07 14:28:26 +0530

    Understanding JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hi, I am a React App</h1>
-    </div>
+    // <div className="App">
+    //   <h1> Hi, I am a React App</h1>
+    // </div>
+    //React.createElement('div', null, 'h1', 'Hi I am a React App')
+    React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Iam a React App')) //writing the code like this is cumbersome 
+    //Hence we choose above jsx code.
   );
 }
 
