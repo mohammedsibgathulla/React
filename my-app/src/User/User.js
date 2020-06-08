ccef657352895ca51e4711ed098b2b8c6d9a8c4e
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-08 08:18:20 +0530

    Understatnding The Children Prop

--- my-app/src/User/User.js
+++ my-app/src/User/User.js
@@ -1,6 +1,9 @@
 import React from 'react';
 const user = (props) => { //We have implemented ES6 function
-    return <p>I'm a {props.name} and I am {props.age} years old</p>
+    return <div>
+        <p>I'm a {props.name} and I am {props.age} years old</p>
+        <p>{props.children}</p>
+    </div>
 }
 
 export default user;
