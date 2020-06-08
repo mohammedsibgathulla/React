e01cf85efd46f01bb9c24e3da59563584dd70da9
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-08 07:28:30 +0530

    Creating A Functional Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import User from './User/User';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render(){
+   return ( <div className="App">
       <h1> Hi, I am a React App</h1>
       <h1> You can add one more header here</h1>
       <p>This is working great ! </p>
+      <User />
     </div>
     //<h1>Hi</h1> We can't do this here our JSX expression must have one root element
     //React.createElement('div', null, 'h1', 'Hi I am a React App')
     //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, Iam a React App')) //writing the code like this is cumbersome 
     //Hence we choose above jsx code.
-  );
+   );
+  }
 }
 
 export default App;
---
+++ my-app/src/User/User.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const user = function() {
+    return <p>I'm a User</p>
+}
+
+export default user;
