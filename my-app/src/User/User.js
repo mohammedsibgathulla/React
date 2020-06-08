59977d8e4508ab9fe0c215a73d9fac57069e499d
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-08 08:04:34 +0530

    Outputting Dynamic Content

--- my-app/src/User/User.js
+++ my-app/src/User/User.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const user = function() {
-    return <p>I'm a User</p>
+    return <p>I'm a User and I am {Math.floor(Math.random() * 30)} years old</p>
 }
 
 export default user;
