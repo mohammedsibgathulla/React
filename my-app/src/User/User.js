52c20d61073d4aa89400b717bb52e313ad94da7c
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-06-11 11:33:19 +0530

    Adding Styling With Stylesheets

--- my-app/src/User/User.js
+++ my-app/src/User/User.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './User.css';
 const user = (props) => { //We have implemented ES6 function
-    return <div>
+    return <div className="User">
         <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old</p>
         <p>{props.children}</p>
         <input type="text" onChange={props.change} value={props.name} />
